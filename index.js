d8cb243c4f494bf3a8aba96b1cd9210897525195
RTdev <77839017+R1TGAMING@users.noreply.github.com>
2024-08-11 14:53:13 +0000

    Module and setup

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(import.meta.dirname + '/public/index.html')
+})
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+})
